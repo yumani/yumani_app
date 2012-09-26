d09d69a3b017e34d16d6952f815908d3402cbb63
yumani <dzmitryg@ruckusmarketing.com>
2012-09-26 19:25:34 +0000

    Added Hello Test words to default recipt

---
+++ cookbooks/yubook/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Yumani"
+maintainer_email "dzmitryg@ruckusmarketing.com"
+license          "All rights reserved"
+description      "Installs/Configures yubook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "yubook::default","Prints Hello Test"
