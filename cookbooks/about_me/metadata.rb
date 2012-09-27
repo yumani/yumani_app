52c16a7fa145f89c7a07d23d9c401debab69754b
yumani <dzmitryg@ruckusmarketing.com>
2012-09-27 15:09:01 +0000

    fix mistake

--- cookbooks/about_me/metadata.rb
+++ cookbooks/about_me/metadata.rb
@@ -6,4 +6,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "rightscale"
-recipe "about_me:default","Prints my name and several things output"
+recipe "about_me::default","Prints my name and several things output"
