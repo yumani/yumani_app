425db258dc314baab29fdec3f3ec440760d9cfc9
yumani <dzmitryg@ruckusmarketing.com>
2012-09-27 16:02:49 +0000

    /opt/development/cookbooks/

--- cookbooks/about_me/attributes/me.rb
+++ cookbooks/about_me/attributes/me.rb
@@ -1,5 +1,5 @@
 set[:about_me][:first_name]= "Dmitry"
 default[:about_me][:favorite][:food] = "pizza"
 default[:about_me][:favorite][:drink] = "water"
-defailt[:about_me][:favorite][:hobby]= "running"
+default[:about_me][:favorite][:hobby]= "running"
 
