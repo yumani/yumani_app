f9c6de31899dd93931f958010362b14e779f43f0
yumani <dzmitryg@ruckusmarketing.com>
2012-09-27 15:34:40 +0000

    more

--- cookbooks/about_me/attributes/me.rb
+++ cookbooks/about_me/attributes/me.rb
@@ -1,4 +1,4 @@
-set[:about_me][:first_name]= "<INSERT_YOUR_NAME_HERE>"
+set[:about_me][:first_name]= "Dmitry"
 default[:about_me][:favorite][:food] = "pizza"
 default[:about_me][:favorite][:drink] = "water"
 defailt[:about_me][:favorite][:hobby]= "running"
--- cookbooks/about_me/recipes/default.rb
+++ cookbooks/about_me/recipes/default.rb
@@ -1,14 +1,13 @@
-#
 # Cookbook Name:: about_me
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Yumani LLC
 #
-# All rights reserved - Do Not 
-rightscale_marker:begin
+# All rights reserved - Do Not Restribute 
+rightscale_marker :begin
 first_name = node[:about_me][:first_name]
 food = node[:about_me][:favorite][:food]
 drink = node[:about_me][:favorite][:drink]
 hobby = node[:about_me][:favorite][:hobby]
 log "Hi, My Name is #{first_name} and I like #{food}, #{drink}, #{hobby}"
-rightscale_marker:end
+rightscale_marker :end
