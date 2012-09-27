74d0b22b720b6c63ff229a904674ff0b4e012ecb
yumani <dzmitryg@ruckusmarketing.com>
2012-09-27 15:22:19 +0000

    /opt/development/cookbooks/

--- cookbooks/about_me/recipes/default.rb
+++ cookbooks/about_me/recipes/default.rb
@@ -7,8 +7,8 @@
 # All rights reserved - Do Not 
 rightscale_marker:begin
 first_name = node[:about_me][:first_name]
-food = node[:about_me][:food]
-drink = node[:about_me][:drink]
-hobby = node[:about_me][:hobby]
+food = node[:about_me][:favorite][:food]
+drink = node[:about_me][:favorite][:drink]
+hobby = node[:about_me][:favorite][:hobby]
 log "Hi, My Name is #{first_name} and I like #{food}, #{drink}, #{hobby}"
 rightscale_marker:end
